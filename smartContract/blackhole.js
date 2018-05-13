f2ede8fbbf2749803bc3125a44cd83bd96688f4b
Nate <supermanheng21@gmail.com>
2018-05-14 00:05:30 +0800

    finished

--- smartContract/blackhole.js
+++ smartContract/blackhole.js
@@ -1,93 +1,112 @@
 "use strict";
 
-var Binding = function(text) {
+var Whisper = function(text) {
 	if (text) {
 		var obj = JSON.parse(text);
-		this.username = obj.username;
+		this.content = obj.content;
 		this.address = obj.address;
+        this.time = obj.time;
+        this.index = obj.index;
 	} else {
-        this.username = "";
+        this.content = "";
         this.address = "";
+        this.time = "";
+        this.index = "";
 	}
 
 };
 
-Binding.prototype = {
+Whisper.prototype = {
 	toString: function () {
 		return JSON.stringify(this);
 	}
 };
 
 var FederationService = function () {
-    LocalContractStorage.defineMapProperty(this, "bindings", {
+    LocalContractStorage.defineMapProperty(this, "whispers", {
         parse: function (text) {
-            return new Binding(text);
+            return new Whisper(text);
         },
         stringify: function (o) {
             return o.toString();
         }
     });
+    LocalContractStorage.defineProperty(this, "size");
+    LocalContractStorage.defineProperty(this, "creator");
 };
 
 FederationService.prototype = {
     init: function () {
         // save the creator
         var from = Blockchain.transaction.from;
-        LocalContractStorage.set("creator", from);
+        this.creator = from;
+        this.size = 0;
     },
 
-    save: function (username) {
-
-        username = username.trim();
-        if (username === ""){
-            throw new Error("empty username");
+    save: function (content) {
+        content = content.trim();
+        if (content === ""){
+            throw new Error("empty content");
         }
 
-
+        var whisper  = new Whisper();
         var from = Blockchain.transaction.from;
-        var binding = this.bindings.get(username);
-        if (binding){
-            throw new Error("username has been occupied");
-        }
 
-        binding = new Binding();
-
-        binding.username = username;
-        binding.address = from;
-
-        this.bindings.put(username, binding);
-
-        return binding;
+        whisper.content = content;
+        whisper.address = from;
+        whisper.time = new Date();
+        whisper.index = this.size;
+        var result = this.whispers.put(this.size, whisper);
+        this.size++;
+        return result;
     },
 
-    get: function (username) {
-        username = username.trim();
-        if ( username === "" ) {
-            throw new Error("empty username")
+    get: function (page, num) {
+        page = page.trim();
+        num = num.trim();
+        var start;
+        var i=0;
+        if (page === "" || num === "") {
+            start = this.size;
+            if (num === "") {
+                num = this.size;
+            }
+
+        } else {
+            start = this.size - (page-1) * num;
         }
-        return this.bindings.get(username);
+
+        var results = [];
+        for (;i<num; start--,i++ ){
+            var result = this.whispers.get(start);
+            if (result == null) {
+                continue; //the element of the index might be deleted
+            }
+            results.push(result)
+        }
+        return results;
     },
 
-    del: function (username) {
-        username = username.trim();
-        if ( username === "" ) {
-            throw new Error("empty username")
+    del: function (index) {
+        index = index.trim();
+        if ( index === "" ) {
+            throw new Error("empty content")
         }
 
-        var binding = this.bindings.get(username);
-        if ( binding.address !== Blockchain.transaction.from  ) {
-            throw new Error("you can not delete the binding")
+        var whisper = this.whispers.get(index);
+        if ( whisper.address === Blockchain.transaction.from || this.creator=== Blockchain.transaction.from ) {
+            this.whispers.del(index);
         }
-
-        this.bindings.del(username);
-
+        else {
+            throw new Error("you can not delete the whisper")
+        }
     },
 
     //in case of someone sends nas to tht contract
     takeout: function (value) {
         var amount = new BigNumber(value);
         //var from = Blockchain.transaction.from;
-        var creator = LocalContractStorage.get("creator");
+        var creator = this.creator;
         var result = Blockchain.transfer(creator, amount);
         return result;
     }
